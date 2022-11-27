05dc7cdc915ca9f74efed6c052353ea4c155da94
vlakondra <kondrashkin.wladimir@yandex.ru>
2022-11-27 13:09:20 +0000

    Change configs

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,37 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // from  https://github.com/stroblp/svelte-persistent-store
 export default defineConfig({
   plugins: [svelte()],
-  server: {
-    // configure vite for HMR with Gitpod
-    hmr: process.env.GITPOD_WORKSPACE_URL
-      ? {
-        // removes the protocol and replaces it with the port we're connecting to
-        host: process.env.GITPOD_WORKSPACE_URL.replace('https://', '3000-'),
-        protocol: 'wss',
-        clientPort: 443
-      }
-      : true
-  },
-  // base:"/ssched/"
+  
+  // server: {
+  //   // configure vite for HMR with Gitpod
+  //   hmr: process.env.GITPOD_WORKSPACE_URL
+  //     ? {
+  //       // removes the protocol and replaces it with the port we're connecting to
+  //       host: process.env.GITPOD_WORKSPACE_URL.replace('https://', '3000-'),
+  //       //host:$GITPOD_WORKSPACE_URL,
+  //       protocol: 'wss',
+  //       clientPort: 443
+  //     }
+  //     : true
+  // },
+
+  vite: {
+    server: {
+      // configure vite for HMR with Gitpod
+      hmr: process.env.GITPOD_WORKSPACE_URL
+        ? {
+            // removes the protocol and replaces it with the port we're connecting to
+            host: process.env.GITPOD_WORKSPACE_URL.replace('https://', '3000-'),
+            protocol: 'wss',
+            clientPort: 443
+          }
+        : true
+    }
+  }
+
+
+
+   //base:"/svelte_route/"
 })
 
 
